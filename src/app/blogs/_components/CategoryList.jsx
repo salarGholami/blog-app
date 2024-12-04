9d055e355ad401ad943b4851641163d00ee0707f
salarGholami <salargholami021@gmail.com>
2024-12-04 04:48:29 +0330

    Fetching Data in Server Components

---
+++ src/app/blogs/_components/CategoryList.jsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+async function CategoryList() {
+  const res = await fetch("http://localhost:5000/api/category/list");
+  const {
+    data: { categories },
+  } = await res.json();
+
+  return (
+    <ul className="space-y-4">
+      <Link href={`/blogs`}>همه</Link>
+      {categories.map((cattegory) => {
+        return (
+          <li key={cattegory._id}>
+            <Link href={`/blogs/category/${cattegory.slug}`}>
+              {cattegory.title}
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+export default CategoryList;
--- src/app/blogs/layout.jsx
+++ src/app/blogs/layout.jsx
@@ -1,3 +1,5 @@
+import CategoryList from "./_components/CategoryList";
+
 export const metadata = {
   title: "بلاگ ها",
 };
@@ -8,7 +10,7 @@ function layout({ children }) {
       <h1 className="text-lg font-bold mb-12">لیست بلاگ ها</h1>
       <div className="grid grid-cols-12 gap-8">
         <div className="col-span-12 lg:col-span-4 xl:col-span-3 text-secondary-500 space-y-4">
-          catgory list
+          <CategoryList />
         </div>
         <div className="col-span-12 lg:col-span-4 xl:col-span-9">
           {children}
