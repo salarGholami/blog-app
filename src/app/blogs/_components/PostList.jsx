50d95ee7c58041251708a90d70633557a73686b0
salarGholami <salargholami021@gmail.com>
2024-12-08 04:45:18 +0330

    Post Interactions

--- src/app/blogs/_components/PostList.jsx
+++ src/app/blogs/_components/PostList.jsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { ClockIcon } from "@heroicons/react/24/outline";
 import Avatar from "@/ui/Avatar";
 import Author from "./Author";
+import PostInteraction from "./PostInteraction";
 
 async function PostList() {
   await new Promise((res) => setTimeout(() => res(), 2000));
@@ -25,8 +26,8 @@ async function PostList() {
               </h2>
             </Link>
             {/* post author - readingTime */}
-            <div className="flex items-center justify-between">
-              <Author {...post.author}/>
+            <div className="flex items-center justify-between mb-4">
+              <Author {...post.author} />
               <div className="flex items-center text-[10px] text-secondary-500">
                 <ClockIcon className="w-4 h-4 stroke-secondary-500 ml-1" />
                 <span className="ml-1">خواندن :</span>
@@ -34,6 +35,7 @@ async function PostList() {
                 <span>دقیقه</span>
               </div>
             </div>
+            <PostInteraction post={post} />
           </div>
         </div>
       ))}
