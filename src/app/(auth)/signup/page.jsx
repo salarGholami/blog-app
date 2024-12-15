a0f9f368c40b03ddef55d971f757235fae66d81d
salarGholami <salargholami021@gmail.com>
2024-12-15 05:19:25 +0330

    Adding React Hook Form

--- src/app/(auth)/signup/page.jsx
+++ src/app/(auth)/signup/page.jsx
@@ -1,21 +1,51 @@
-import TextField from "@/ui/TextField";
-import { useState } from "react";
+"use client";
+import Button from "@/ui/Button";
+import RHFTextField from "@/ui/RHFTextField";
+import { useForm } from "react-hook-form";
 
-export const metadata = {
-  title: "ثبت نام",
-};
+// export const metadata = {
+//   title: "ثبت نام",
+// };
 
 function Signup() {
-  const [ form, setForm]=useState({})
-  
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
   return (
     <div>
-      <h1>ثبت نام</h1>
-      <form action="">
-        <TextField name="name" label="نام و نام خانوادگی" value={} onChange={} isRequired type="text" />
+      <h1 className="text-xl font-bold text-secondary-500 text-center mb-6">
+        ثبت نام
+      </h1>
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-10">
+        <RHFTextField
+          name="name"
+          label="نام و نام خانوادگی"
+          register={register}
+          type="text"
+        />
+        <RHFTextField
+          name="email"
+          label="ایمیل"
+          register={register}
+          type="email"
+          dir="ltr"
+        />
+        <RHFTextField
+          name="password"
+          label="رمز عبور"
+          register={register}
+          type="password"
+          dir="ltr"
+        />
+        <Button type="submit" variant="primary" className="w-full">
+          تایید
+        </Button>
       </form>
     </div>
   );
 }
 
-export default page;
+export default Signup;
